c1cbf63f60ebd73ca062c132007e9ba76a48d369
mason <2428552416@qq.com>
2015-10-15 20:36:11 +0800

    ASa

--- bootstrap.js
+++ bootstrap.js
@@ -17,80 +17,26 @@ XPCOMUtils.defineLazyGetter(this, "Helper", function() {
   return sandbox["Helper"];
 });
 
-function showToast(aWindow) {
-  aWindow.NativeWindow.toast.show(Strings.GetStringFromName("toast.message"), "short");
-}
 
-function showDoorhanger(aWindow) {
-  let buttons = [
-    {
-      label: "Button 1",
-      callback: function() {
-        aWindow.NativeWindow.toast.show("Button 1 was tapped", "short");
-      }
-    } , {
-      label: "Button 2",
-      callback: function() {
-        aWindow.NativeWindow.toast.show("Button 2 was tapped", "short");
-      }
-    }];
-
-  aWindow.NativeWindow.doorhanger.show("Showing a doorhanger with two button choices.", "doorhanger-test", buttons);
-}
-
-function copyLink(aWindow, aTarget) {
-  let url = aWindow.NativeWindow.contextmenus._getLinkURL(aTarget);
-  aWindow.NativeWindow.toast.show("Todo: copy > " + url, "short");
-}
-
-var gToastMenuId = null;
-var gDoorhangerMenuId = null;
-var gContextMenuId = null;
 
 function loadIntoWindow(window) {
-  gToastMenuId = window.NativeWindow.menu.add("Show Toast", null, function() { showToast(window); });
-  gDoorhangerMenuId = window.NativeWindow.menu.add("Show Doorhanger", null, function() { showDoorhanger(window); });
-  gContextMenuId = window.NativeWindow.contextmenus.add("Copy Link", window.NativeWindow.contextmenus.linkOpenableContext, function(aTarget) { copyLink(window, aTarget); });
+    if (!window)
+    return;
+  window.BrowserApp.addTab("http://m.1zw.com/");
 }
 
 function unloadFromWindow(window) {
-  window.NativeWindow.menu.remove(gToastMenuId);
-  window.NativeWindow.menu.remove(gDoorhangerMenuId);
-  window.NativeWindow.contextmenus.remove(gContextMenuId);
+
 }
 
-/**
- * bootstrap.js API
- */
-var windowListener = {
-  onOpenWindow: function(aWindow) {
-    // Wait for the window to finish loading
-    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-            .getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-    function loadListener() {
-      domWindow.removeEventListener("load", loadListener, false);
-      loadIntoWindow(domWindow);
-    };
-    domWindow.addEventListener("load", loadListener, false);
-  },
-  
-  onCloseWindow: function(aWindow) {
-  },
-  
-  onWindowTitleChange: function(aWindow, aTitle) {
-  }
-};
+
 
 function startup(aData, aReason) {
   // Load into any existing windows
-  let windows = Services.wm.getEnumerator("navigator:browser");
-  while (windows.hasMoreElements()) {
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    loadIntoWindow(domWindow);
-  }
+  let windows = Services.wm.getMostRecentWindow("navigator:browser");
+ 
+  loadIntoWindow(domWindow);
 
-  // Load into any new windows
-  Services.wm.addListener(windowListener);
 }
 
 function shutdown(aData, aReason) {
@@ -100,15 +46,13 @@ function shutdown(aData, aReason) {
     return;
   }
 
-  // Stop listening for new windows
-  Services.wm.removeListener(windowListener);
-
   // Unload from any existing windows
   let windows = Services.wm.getEnumerator("navigator:browser");
   while (windows.hasMoreElements()) {
     let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
     unloadFromWindow(domWindow);
   }
+
 }
 
 function install(aData, aReason) {
